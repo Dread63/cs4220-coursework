6d79bfda9a32710e3a985bdce1908ed116710658
Josh Douglas <123434357+Dread63@users.noreply.github.com>
2025-09-09 13:17:26 -0600

    Server is transmitting file to client

--- project1/cleint.c
+++ project1/cleint.c
@@ -3,33 +3,57 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 2222
-#define SERVER_IP "142.250.69.238"
+#define SERVER_IP "127.0.0.1"
 #define DATA_FRAME_SIZE 1024
 
 int main() {
 
-    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int file_data;
 
     struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
+    memset(&address, 0, sizeof address);
     address.sin_port = htons(PORT);
     address.sin_family = AF_INET;
 
     // Convert string address to unit_32 bit value
     inet_pton(AF_INET, SERVER_IP, &address.sin_addr.s_addr);
 
-    int result = connect(socketFD, (struct sockaddr*)&address, sizeof address);
+    int result = connect(socket_fd, (struct sockaddr*)&address, sizeof address);
 
     if (result == 0) {
 
         printf("Connection succesful\n");
     }
 
-    FILE *outputFiels = fopen("client_files/received.txt", "wb");
+    else {
+        printf("Error Connecting");
+    }
 
-    
+    FILE *output_file = fopen("client_files/received.txt", "wb");
+    char buffer[DATA_FRAME_SIZE + 1] = {0};
+
+    while(1) {
+
+        file_data = recv(socket_fd, buffer, DATA_FRAME_SIZE + 1, 0);
+
+        if (file_data <= 0) {
+            break;
+            return -1;
+        }
+
+        size_t written = fwrite(buffer + 1, 1, DATA_FRAME_SIZE, output_file);
+        if (written != DATA_FRAME_SIZE) {
+            perror("fwrite");
+            return -1;
+        }
+
+        puts("Done writing output file");
+        break;
+    }
 
     return 0;
 }
--- project1/server.c
+++ project1/server.c
@@ -6,9 +6,16 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define PORT "2222"
 #define MAX_BACKLOG 5
+#define FRAME_SIZE 1024
+
+int send_file_data(int socket_fd, const char *filepath);
+
 
 int main(void) {
 
@@ -20,6 +27,8 @@ int main(void) {
 
     struct addrinfo hints;
     struct addrinfo *servinfo, *p;
+
+    socklen_t sin_size;
 
     // Address of client connecting to server
     struct sockaddr_storage client_addr;
@@ -59,13 +68,68 @@ int main(void) {
     // Free up memory dedicated to the server info structure
     freeaddrinfo(servinfo);
 
-    if (listen(socket_fd, MAX_BACKLOG) != 0) {
+    if (listen(socket_fd, MAX_BACKLOG) == -1) {
 
         perror("Listen error: Too many connections in queue");
         exit(1);
     }
+    printf("Listening....\n");
 
+    // Accept connection from client
+    sin_size = sizeof client_addr;
+    new_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &sin_size);
+    if (new_fd == -1) {
+        perror("accept");
+        close(socket_fd);
+        return 1;
+    }
+    printf("Connection accepted\n");
 
+    // Reading file
+    send_file_data(new_fd, "inputfile.txt");
 
     return 0;
+}
+
+int send_file_data(int socket_fd, const char *filepath) {
+
+    FILE *data_file = fopen(filepath, "rb");
+
+    uint8_t current_sequence = 0;
+    uint8_t current_frame[FRAME_SIZE + 1];
+
+    if (data_file == NULL) {
+
+        perror("datafile");
+        return -1;
+    }
+
+    // Buffer used to read inputfile.txt
+    char buffer[FRAME_SIZE] = {0};
+
+    size_t bytes_read;
+    while((bytes_read = fread(buffer, 1, FRAME_SIZE,  data_file)) > 0) {
+
+        current_frame[0] = current_sequence;
+
+        // Copy all data from file after the first sequence bit
+        memcpy(&current_frame[1], buffer, bytes_read);
+
+        size_t to_send = 1 + bytes_read;
+        if(send(socket_fd, current_frame, to_send, 0) == -1) {
+            perror("send");
+            exit(1);
+        }
+
+        bzero(buffer, FRAME_SIZE);
+        //uint8_t ack;
+        //recv(clientsocket, ack, sizeof(ack), 0);
+
+        // Continue re-transmitting current frame until ack matches
+        //while (ack != current_frame) {
+
+          //  write(socket, current_frame, FRAME_SIZE + 1);
+            //recv(clientsocket, ack, sizeof(ack), 0);
+        //}
+    }
 }
